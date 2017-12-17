4ed4e10b250668d8bbb4cceb6f267714c84046de
Guanhui Guan <guanhui.guan@columbia.edu>
2017-12-16 22:15:29 -0500

    Update Kth_Largest_Element_in_an_Array.java

--- Kth_Largest_Element_in_an_Array.java
+++ Kth_Largest_Element_in_an_Array.java
@@ -1,25 +1,50 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int block = 0;
-        int nbrs = 0;
+    public int findKthLargest(int[] nums, int k) {
+        // shuffle once to let things get random
+        shuffle(nums);
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    block++;
-                    // down neighbor
-                    if(i+1 < m && grid[i+1][j] == 1){
-                        nbrs++;
-                    }
-                    // right neighbor
-                    if(j+1 < n && grid[i][j+1] == 1){
-                        nbrs++;
-                    }
-                }
+        k = nums.length - k;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end){
+            int i = partition(nums, start, end);
+            if(i == k)  return nums[k];
+            if(i < k){
+                start = i+1;
+            }
+            else{
+                end = i-1;
             }
         }
-        return 4*block - 2*nbrs;
+        return nums[start];
+    }
+    
+    public void shuffle(int[] nums){
+        final Random random = new Random();
+        for(int i = 0; i < nums.length; i++){
+            int j = random.nextInt(i+1);
+            swap(nums, i, j);
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public int partition(int[] nums, int start, int end){
+        int i = start;
+        int j = i - 1;
+        while(i < end){
+            if(nums[i] < nums[end]){
+                j++;
+                swap(nums, i, j);
+            }
+            i++;
+        }
+        j++;
+        swap(nums, j, end);
+        return j;
     }
 }
